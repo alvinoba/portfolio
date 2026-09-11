478bc981554c90faafa9e7bf39fe2eadc7336d83
Alvin <alvinoba0607@Alvin.localdomain>
2026-09-11 18:25:01 -0400

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [],
+	integrations: [
+	],
+	site: 'https://alvinoba.github.io',
+  	base: '/portfolio',
 });
